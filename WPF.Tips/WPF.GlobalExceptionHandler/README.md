b62195b6abace46c1bf89c6c5c07b4e64de07cb2
shimanamisan <mikan.sup.all@gmail.com>
2024-09-08 23:38:15 +0900

    update

---
+++ WPF.Tips/WPF.GlobalExceptionHandler/README.md
@@ -0,0 +1,9 @@
+# 概要
+
+WPF で例外処理を共通化する方法を記述したサンプルアプリケーションです。
+
+![custom-exception](https://github.com/user-attachments/assets/46e555f4-8b32-4e9b-9957-5d406a128737)
+
+# 解説記事
+
+- [【WPF】例外処理を共通化する](https://blog.hn-pgtech.com/2023-04-14/)
---
+++ WPF.Tips/WPF.GlobalExceptionHandler/WPF.GlobalExceptionHandler/Exceptions/WarningException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WPF.GlobalExceptionHandler
+{
+    public sealed class WarningException : BaseException
+    {
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="message">出力メッセージ</param>
+        /// <param name="exception">インナーエクセプション</param>
+        public WarningException(string message, Exception exception) : base(message, exception)
+        { }
+
+        /// <summary>
+        /// 例外の区分
+        /// </summary>
+        public override ExceptonKind Kind => ExceptonKind.Warning;
+    }
+}
